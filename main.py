a24c459d04203f30795c62462094f40e306ec689
Mushhead <gitavi2108@gmail.com>
2022-04-23 09:43:00 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import lstm7
+from cvcapture import run,store,train
+
+import cv2
+import numpy as np
+import media as mp
+import time
+from matplotlib import pyplot as plt
+import os
+import preprocess6
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('PyCharm')
+
+
+
+    # store()
+    # run()
+    # train()
+    # preprocess6.training_testing()
+    lstm7.test()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
